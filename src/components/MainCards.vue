9aabce2f7864b76248c647e9978022c12963d723
Jaime Corona Jr <mitos2018_78@outlook.com>
2024-10-05 18:40:00 -0500

    Modified a few components

--- src/components/MainCards.vue
+++ src/components/MainCards.vue
@@ -5,8 +5,8 @@ import MainCardSingle from './MainCardSingle.vue'
 </script>
 
 <template>
-<div class="grid grid-cols-2 gap-10 p-10">
-    <MainCardSingle v-for="card in 10" :key="card"></MainCardSingle>
+<div class="grid grid-cols-3 gap-10 p-10">
+    <MainCardSingle v-for="card in 18" :key="card"></MainCardSingle>
 </div>
 
 </template>
--- src/router.js
+++ src/router.js
@@ -1,10 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomePage from '@/views/HomePage.vue'
+import CardDetails from '@/views/CardDetails.vue'
+import LoginPage from '@/views/LoginPage.vue'
+import SettingsPage from '@/views/SettingsPage.vue'
+
+  
 
 const routes = [
-  { path: '/', name: 'Home', component: HomePage },
-  { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
+  { path: '/Project5/', name: 'Home', component: HomePage },
+  { path: '/Project5,other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
+  { path: '/Project5/employees/:id', name: 'CardDetails', component: CardDetails },
+  { path: '/Project5/login', name: 'LoginPage', component: LoginPage },
+  { path: '/Project5/settings', name: 'SettingsPage'},
 ]
 
 const router = createRouter({
